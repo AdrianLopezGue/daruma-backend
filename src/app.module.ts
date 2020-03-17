31a5fcf028c8ceea9f4c93d2c5002ee0a18b3cb9
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-17 21:39:13 +0100

    initial files modified to this project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
 
+import { BootstrapModule } from './bootstrap.module';
+
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BootstrapModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('DARUMA');
+
+  const options = new DocumentBuilder()
+    .setTitle('DARUMA')
+    .setDescription('Shared expenses manager')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0', () => {
+    logger.log('Started at http://localhost:3000/api');
+  });
 }
 bootstrap();
