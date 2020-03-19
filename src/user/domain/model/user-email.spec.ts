4802128b88b4b560a83e6938019ec5608ec838b5
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-19 23:39:09 +0100

    user email modified

--- src/user/domain/model/user-email.spec.ts
+++ src/user/domain/model/user-email.spec.ts
@@ -1,14 +1,14 @@
 import { EmptyUseremailError } from '../exception/empty-useremail.error';
 import { UserEmail } from './user-email';
 
-describe('Fullname', () => {
-  it('creates a new fullname', () => {
-    const fullname = UserEmail.fromString('john@john.es');
+describe('Useremail', () => {
+  it('creates a new email', () => {
+    const email = UserEmail.fromString('john@john.es');
 
-    expect(fullname.value).toBe('john@john.es');
+    expect(email.value).toBe('john@john.es');
   });
 
-  it('expects the fullname', () => {
+  it('expects the useremail to fail', () => {
     expect(() => UserEmail.fromString('')).toThrow(EmptyUseremailError);
   });
 });
