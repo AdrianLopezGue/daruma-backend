9bddb6532b623e6e6c297e1d5b82a448f2cf9dad
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-20 19:54:46 +0100

    changes in application layer

---
+++ src/group/application/command/index.ts
@@ -0,0 +1,2 @@
+export * from './create-group.command';
+export * from './change-group-name.command';
---
+++ src/group/application/handler/index.ts
@@ -0,0 +1,4 @@
+import { CreateGroupHandler } from './create-group.handler';
+import { ChangeGroupNameHandler } from './change-group-name.handler';
+
+export const CommandHandlers = [CreateGroupHandler, ChangeGroupNameHandler];
