dcd529802d0ee084fd282d3aaae952b2eb048514
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-25 11:35:09 +0100

    deploy in firebase done

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+
+import { AppModule } from './app.module';
+
+const server = express();
+
+export const createNestServer = async expressInstance => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+  .then(v => console.log('Nest Ready'))
+  .catch(err => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
