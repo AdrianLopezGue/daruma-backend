6069dbebd00932b0cbca1d9421a3db1450d68c00
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-28 13:41:12 +0100

    provide GROUPS in group provider

--- src/group/infrastructure/group.provider.ts
+++ src/group/infrastructure/group.provider.ts
@@ -3,6 +3,8 @@ import { Connection } from 'mongoose';
 import { CHECK_UNIQUE_GROUP_NAME } from '../domain/services/check-unique-group-name.service';
 import { GROUP_MODEL, GroupSchema } from './read-model/schema/group.schema';
 import { CheckUniqueGroupNameFromReadModel } from './service/check-unique-group-name.service';
+import { GROUPS } from '../domain/repository/index';
+import { GroupDatabase } from './database';
 
 export const GroupProviders = [
   {
@@ -10,6 +12,10 @@ export const GroupProviders = [
     useFactory: (connection: Connection) =>
       connection.model('Group', GroupSchema),
     inject: ['DATABASE_CONNECTION'],
+  },
+  {
+    provide: GROUPS,
+    useClass: GroupDatabase,
   },
   {
     provide: CHECK_UNIQUE_GROUP_NAME,
