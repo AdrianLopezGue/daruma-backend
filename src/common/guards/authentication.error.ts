a02f54bccaf8d3e34bdb2c009dc4f3ab111c680c
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-28 18:05:22 +0100

    files moved to infrastructure deleted

--- src/common/guards/authentication.error.ts
+++
@@ -1,7 +0,0 @@
-export class AuthenticationError extends Error {
-    public static withString(): AuthenticationError {
-        return new AuthenticationError(
-        `Error during authentication token id`,
-        );
-    }
-  }
