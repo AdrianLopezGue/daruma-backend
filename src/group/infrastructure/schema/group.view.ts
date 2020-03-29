739edcd98e3ecff1b3b2c6efa917611ac0ace989
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-29 16:53:17 +0200

    constructor added in view

--- src/group/infrastructure/schema/group.view.ts
+++ src/group/infrastructure/schema/group.view.ts
@@ -3,4 +3,11 @@ export class GroupView {
     name: string;
     currencyCode: string;
     idOwner: string;
+
+    constructor(idGroup: string, name: string, currencyCode: string, idOwner: string){
+        this.idGroup = idGroup;
+        this.name = name;
+        this.currencyCode = currencyCode;
+        this.idOwner = idOwner;
+    }
 }
