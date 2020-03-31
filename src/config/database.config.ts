e03e9d721f172d06640c8fdd33082539237cf9cf
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-31 13:04:22 +0200

    configuration files for mongo and eventstore added

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,3 @@
 export default {
-  url: process.env.DATABASE_URL,
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  storageBucket: process.env.STORAGE_BUCKET
-};
+    url: process.env.DATABASE_URL || 'mongodb://localhost/database',
+  };
