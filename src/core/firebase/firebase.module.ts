30311b3a2dcf92386dc27b4c06edaaa9c1d82b6f
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-31 13:03:02 +0200

    firestore rename to get back eventstore and mongo

---
+++ src/core/firebase/firebase.module.ts
@@ -0,0 +1,10 @@
+import { FirebaseDatabase } from './firebase';
+import { Module } from '@nestjs/common';
+
+
+@Module({
+  providers: [FirebaseDatabase],
+  exports: [FirebaseDatabase],
+})
+
+export class FirebaseModule {}
