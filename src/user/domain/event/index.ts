32efe8a805651b6afdd03bec84fd58ee2c54c659
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-04 11:06:42 +0200

    deleted unused events

--- src/user/domain/event/index.ts
+++ src/user/domain/event/index.ts
@@ -1,14 +1,8 @@
-import { UserWasRegistered } from './user-was-registered.event';
-import { UsernameWasChanged } from './username-was-changed.event';
-import { UseremailWasChanged } from './useremail-was-changed.event';
+import { UserWasCreated } from './user-was-created.event';
 
-export { UserWasRegistered, UsernameWasChanged, UseremailWasChanged };
+export { UserWasCreated };
 
 export const userEventHandlers = {
-  UserWasRegistered: (id: string, name: string, alias: string) =>
-    new UserWasRegistered(id, name, alias),
-  UsernameWasChanged: (id: string, name: string) =>
-    new UsernameWasChanged(id, name),
-  UseremailWasChanged: (id: string, mail: string) =>
-    new UseremailWasChanged(id, mail),
+  UserWasCreated: (id: string, name: string, alias: string) =>
+    new UserWasCreated(id, name, alias),
 };
