54da132c75bd9436c5c5294558d10e48410d6e38
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-04 19:34:10 +0200

    tests pass now

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Global, Module } from '@nestjs/common';
 
 import { BootstrapModule } from './bootstrap.module';
 import { GroupModule } from './group/infrastructure/group.module';
+import { MemberModule } from './member/infrastructure/member.module';
+import { UserModule } from './user/infrastructure/user.module';
 
 @Global()
 @Module({
-  imports: [BootstrapModule, GroupModule],
+  imports: [BootstrapModule, GroupModule, UserModule, MemberModule],
 })
 export class AppModule {}
