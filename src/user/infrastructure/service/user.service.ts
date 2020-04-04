d5bab49113537deef3669f5faeb0da12b8586172
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-04 11:25:46 +0200

    user infrastructure layer created

---
+++ src/user/infrastructure/service/user.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { Model } from 'mongoose';
+
+import {
+  CreateUserCommand
+} from '../../application/command';
+import { USER_MODEL, UserView } from '../read-model/schema/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly commandBus: CommandBus,
+    @Inject(USER_MODEL) private readonly userModel: Model<UserView>,
+  ) {}
+
+  async createUser(id: string, name: string, email: string) {
+    return this.commandBus.execute(new CreateUserCommand(id, name, email));
+  }
+}
---
+++ src/user/infrastructure/user.module.ts
@@ -0,0 +1,32 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+
+
+import { EventStore } from '../../core/eventstore/eventstore';
+import { EventStoreModule } from '../../core/eventstore/eventstore.module';
+import { DatabaseModule } from '../../core/database/database.module';
+import { ProjectionHandlers } from './read-model/projection/index';
+import { CommandHandlers } from '../application/handler/index';
+import { UserService } from './service/user.service';
+import { UserEventStore } from './eventstore/user.event-store';
+import { userEventHandlers } from '../domain/event/index';
+import { UserProviders } from './user.provider';
+
+@Module({
+  controllers: [],
+  imports: [CqrsModule, DatabaseModule, EventStoreModule.forRoot()],
+  providers: [
+    ...CommandHandlers,
+    ...ProjectionHandlers,
+    ...UserProviders,
+    UserService,
+    UserEventStore,
+  ],
+})
+export class UserModule implements OnModuleInit {
+  constructor(private readonly eventStore: EventStore) {}
+
+  onModuleInit() {
+    this.eventStore.addEventHandlers(userEventHandlers);
+  }
+}
