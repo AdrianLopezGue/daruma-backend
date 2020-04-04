a7e07ea41d323b79f378db16799e409c6df5643f
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-04 13:20:53 +0200

    check unique user email service added in user provider

--- src/user/infrastructure/user.provider.ts
+++ src/user/infrastructure/user.provider.ts
@@ -3,6 +3,8 @@ import { Connection } from 'mongoose';
 import { USER_MODEL, UserSchema } from './read-model/schema/user.schema';
 import { UserEventStore } from './eventstore/user.event-store';
 import { USERS } from '../domain/repository/users';
+import { CHECK_UNIQUE_USER_EMAIL } from '../domain/services/check-unique-user-email.service';
+import { CheckUniqueUserEmailFromReadModel } from './service/check-unique-user-email.service';
 
 export const UserProviders = [
   {
@@ -14,5 +16,9 @@ export const UserProviders = [
   {
     provide: USERS,
     useClass: UserEventStore,
-  }
+  },
+  {
+    provide: CHECK_UNIQUE_USER_EMAIL,
+    useClass: CheckUniqueUserEmailFromReadModel,
+  },
 ];
