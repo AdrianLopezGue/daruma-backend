1c07c27808d9004e5a43d9cce963bdda3ca4b0ba
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-15 21:24:04 +0200

    payer and debtor delete, they belong to bill as vo

--- src/receipt/domain/repository/receipts.ts
+++
@@ -1,8 +0,0 @@
-import { ReceiptId } from '../model/receipt-id';
-import { Receipt } from '../model/receipt';
-
-export interface Receipts {
-  find(receiptId: ReceiptId): Promise<Receipt> | null;
-  get(receiptId: ReceiptId): Promise<Receipt>;
-  save(receipt: Receipt): void;
-}
