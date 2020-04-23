9e074e7aaf38872a6b65fb0f2ba569590e03d52c
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-23 21:14:39 +0200

    Naming problems fixed in domain layer

--- src/transaction/domain/repository/transactions.ts
+++ src/transaction/domain/repository/transactions.ts
@@ -1,14 +1,13 @@
 import { TransactionId } from '../model/transaction-id';
-import { DepositTransactionWasCreated } from '../event/deposit-transaction-was-created';
 import { DebtTransaction } from '../model/debt-transaction';
 import { DepositTransaction } from '../model/deposit-transaction';
 
 export interface Transactions {
-  findDepositTransaction(transactionId: TransactionId): Promise<DepositTransactionWasCreated> | null;
-  getDepositTransaction(transactionId: TransactionId): Promise<DepositTransactionWasCreated>;
+  findDepositTransaction(transactionId: TransactionId): Promise<DepositTransaction> | null;
+  getDepositTransaction(transactionId: TransactionId): Promise<DepositTransaction>;
   saveDepositTransaction(depositTransaction: DepositTransaction): void;
 
   findDebtTransaction(transactionId: TransactionId): Promise<DebtTransaction> | null;
   getDebtTransaction(transactionId: TransactionId): Promise<DebtTransaction>;
-  saveDebtTransaction(depositTransaction: DebtTransaction): void;
+  saveDebtTransaction(debtTransaction: DebtTransaction): void;
 }
