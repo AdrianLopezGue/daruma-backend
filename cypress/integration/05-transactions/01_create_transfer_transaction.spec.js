b6e74eb90e970f83a669ec4d191bc0c7b508ffab
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-24 18:20:08 +0200

    Transfer Transaction tests added and passed

---
+++ cypress/integration/05-transactions/01_create_transfer_transaction.spec.js
@@ -0,0 +1,40 @@
+import * as uuid from 'uuid';
+
+describe('POST /transactions', () => {
+
+  beforeEach(() => {
+    cy.fixture('users.json').as('users');
+    cy.fixture('groups.json').as('groups');
+    cy.fixture('bills.json').as('bills');
+    cy.fixture('transactions.json').as('transactions');
+  });
+
+  const post = (auth, sender, beneficiary, transaction) =>
+    cy.request({
+      method: 'POST',
+      url: 'transactions',
+      auth: { bearer: auth },
+      body: {
+        transactionId: transaction.id,
+        senderId: sender.id,
+        beneficiaryId: beneficiary.id,
+        money: transaction.money,
+        currencyCode: transaction.currencyCode,
+        idUser: sender.id
+      },
+      failOnStatusCode: false,
+    });
+
+  it('Validate the status code', function() {
+    post(this.users.johndoe.id, this.users.johndoe, this.users.tommytoe, this.transactions.example)
+      .its('status')
+      .should('equal', 204);
+  });
+
+  it('Validate user validation', function() {
+    const otherUser = uuid.v4();
+    post(otherUser, this.users.johndoe, this.users.tommytoe, this.transactions)
+      .its('status')
+      .should('equal', 403);
+  });
+});
