734c6cf983c9ebde585972f00cca37022e291a7d
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-04-24 17:30:25 +0200

    Transfer transaction added in application layer

---
+++ src/transaction/application/handler/create-transfer-transaction.handler.ts
@@ -0,0 +1,37 @@
+import { Inject } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+import { Transactions } from '../../domain/repository/transactions';
+import { TRANSACTIONS } from '../../domain/repository/index';
+import { TransactionId } from '../../domain/model/transaction-id';
+import { MemberId } from '../../../member/domain/model/member-id';
+import { BillCurrencyUnit } from '../../../bill/domain/model/bill-currency-unit';
+import { GroupCurrencyCode } from '../../../group/domain/model/group-currency-code';
+import { BillAmount } from '../../../bill/domain/model/bill-amount';
+import { CreateTransferTransactionCommand } from '../command/create-transfer-transaction.command';
+import { TransferTransaction } from '../../domain/model/transfer-transaction';
+
+@CommandHandler(CreateTransferTransactionCommand)
+export class CreateTransferTransactionHandler
+  implements ICommandHandler<CreateTransferTransactionCommand> {
+  constructor(@Inject(TRANSACTIONS) private readonly transactions: Transactions) {}
+
+  async execute(command: CreateTransferTransactionCommand) {
+    const transactionId = TransactionId.fromString(command.transactionId);
+    const senderId = MemberId.fromString(command.senderId);
+    const beneficiaryId = MemberId.fromString(command.beneficiaryId);
+    const amount = BillAmount.withMoneyAndCurrencyCode(
+        BillCurrencyUnit.fromNumber(command.money),
+        GroupCurrencyCode.fromString(command.currencyCode),
+    );
+
+    const transaction = TransferTransaction.add(
+        transactionId,
+        senderId,
+        beneficiaryId,
+        amount,
+      );
+
+    this.transactions.saveTransferTransaction(transaction);
+  }
+}
--- src/transaction/application/handler/index.ts
+++ src/transaction/application/handler/index.ts
@@ -1,7 +1,9 @@
 import { CreateDepositTransactionHandler } from './create-deposit-transaction.handler';
 import { CreateDebtTransactionHandler } from './create-debt-transaction.handler';
+import { CreateTransferTransactionHandler } from './create-transfer-transaction.handler';
 
 export const CommandHandlers = [
   CreateDepositTransactionHandler,
   CreateDebtTransactionHandler,
+  CreateTransferTransactionHandler,
 ];
