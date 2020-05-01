99767114e4e45cd591b560151268827f1de07151
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-05-01 15:39:55 +0200

    Added check user from read model in members aggregate

---
+++ src/member/infrastructure/service/check-user-in-group.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { Model } from 'mongoose';
+import { MEMBER_MODEL, MemberView } from '../read-model/schema/member.schema';
+import { MemberId } from '../../domain/model/member-id';
+import { GroupId } from '../../../group/domain/model/group-id';
+import { UserId } from '../../../user/domain/model/user-id';
+import { CheckUserInGroup } from '../../domain/services/check-user-in-group.service';
+
+@Injectable()
+export class CheckUserInGroupFromReadModel implements CheckUserInGroup {
+  constructor(
+    @Inject(MEMBER_MODEL) private readonly memberModel: Model<MemberView>,
+  ) {}
+
+  async with(userId: UserId, groupId: GroupId): Promise<MemberId> {
+
+    const memberView = await this.memberModel
+      .findOne({ $and: [{ groupId: '' + groupId + '' }, { userId: '' + userId + '' }] })
+      .exec();
+
+    if (memberView === null) {
+      return null;
+    }
+
+    return MemberId.fromString(memberView.id);
+  }
+}
