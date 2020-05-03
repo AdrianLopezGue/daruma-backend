94dd2906606a43dea2e82a1a9ec4212a0a39d156
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-05-03 17:58:16 +0200

    Fix problem retrieving group bills

--- src/bill/infrastructure/service/bill.service.ts
+++ src/bill/infrastructure/service/bill.service.ts
@@ -7,11 +7,15 @@ import { CreateBillCommand } from '../../application/command/create-bill.command
 import { ParticipantDto } from '../dto/bill.dto';
 import { RemoveBillCommand } from '../../application/command/remove-bill.command';
 import { GroupIdNotFoundError } from '../../../group/domain/exception/group-id-not-found.error';
+import { GROUPS, Groups } from '../../../group/domain/repository/index';
+import { GroupId } from '../../../group/domain/model/group-id';
+import { Group } from '../../../group/domain/model/group';
 @Injectable()
 export class BillService {
   constructor(
     private readonly commandBus: CommandBus,
     @Inject(BILL_MODEL) private readonly billModel: Model<BillView>,
+    @Inject(GROUPS) private readonly groups: Groups
   ) {}
 
   async createBill(
@@ -46,9 +50,10 @@ export class BillService {
 
   async getBills(groupId: string): Promise<BillView[]> {
     const result = await this.billModel.find({ groupId: groupId }).exec();
+    const group = await this.groups.find(GroupId.fromString(groupId));
 
-    if (result.length === 0) {
-      throw new GroupIdNotFoundError(groupId);
+    if (!(group instanceof Group) || group.isRemoved) {
+      throw GroupIdNotFoundError.withString(groupId);
     }
 
     return result;
